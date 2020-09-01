fb6aec0264f91cd9ea3446edc1d30d2339bd0d98
Charles Collins <coll5631@stthomas.edu>
2020-09-01 15:53:28 -0500

    Rearrange functions

--- get_shoe_data.py
+++ get_shoe_data.py
@@ -10,6 +10,7 @@ detail_expressions = {
     'Type':  'product--meta__sub-title.*'
 }
 
+
 def get_detail(shoe_page, expression):
     """
     Get a specific detail about the shoe
@@ -27,22 +28,22 @@ def get_detail(shoe_page, expression):
     except AttributeError:
         return None
 
-# Get all shoes
-brooks = requests.get('https://www.brooksrunning.com/en_us/shoes/?&sz=300')
-bs = BeautifulSoup(brooks.text, 'html.parser')
 
-# Get links to details for all shoes
-products = bs.find_all('div', {'class': re.compile('.*product-container.*')})
+def get_page(page_url):
+    """
+    Retrieve and parse a web page
 
-product_links = []
+    Args:
+        page_url (str): The url of the page to retrieve and parse
 
-for product in products:
-    product_links.append(product.find('a')['href'])
-
-
-def get_shoe_page(shoe_url):
-    shoe_page = requests.get(product_link)
-    return BeautifulSoup(shoe_page.text, 'html.parser')
+    Returns:
+        obj: Beautiful Soup html object from parse page. If the page cannot be retrieved nothing is returned
+    """
+    try:
+        page = requests.get(page_url)
+    except requests.exceptions.RequestException:
+        return None
+    return BeautifulSoup(page.text, 'html.parser')
 
 
 def get_shoe_specs(shoe_page):
@@ -71,12 +72,24 @@ def get_shoe_specs(shoe_page):
     return features
 
 
+# Get all shoes
+bs = get_page('https://www.brooksrunning.com/en_us/shoes/?&sz=300')
+
+# Get links to details for all shoes
+products = bs.find_all('div', {'class': re.compile('.*product-container.*')})
+
+product_links = []
+
+for product in products:
+    product_links.append(product.find('a')['href'])
+
+
 # Retrieve details for each product
 all_shoes = list()
 loops = 0
 loop_max = 2
 for product_link in product_links:
-    shoe_page = get_shoe_page(product_link)
+    shoe_page = get_page(product_link)
     shoe_features = get_shoe_specs(shoe_page)
     for detail in detail_expressions:
         shoe_features[detail] = get_detail(shoe_page, detail_expressions[detail])
