df66d21fa52f0f552b5b890d2b13a948ff542107
Charles Collins <coll5631@stthomas.edu>
2020-08-31 21:27:45 -0500

    Add code to print shoe details
    Added code that retrieves all links to details for each shoe. Also parses the specs table for each shoe into a dictionary object and prints it to the console

--- get_shoe_data.py
+++ get_shoe_data.py
@@ -1,8 +1,32 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import time
 
-brooks = requests.get('https://www.brooksrunning.com/en_us/shoes/')
+# Get all shoes
+brooks = requests.get('https://www.brooksrunning.com/en_us/shoes/?&sz=300')
 bs = BeautifulSoup(brooks.text, 'html.parser')
 
-print(bs.find_all('div',{'class':re.compile('.*product-container.*')}) )
+# Get links to details for all shoes
+products = bs.find_all('div',{'class':re.compile('.*product-container.*')})
+
+product_links = []
+
+for product in products:
+    product_links.append(product.find('a')['href'])
+
+# Retrieve details for each product
+for product_link in product_links:
+    product_page = requests.get(dproduct_link)
+    bs = BeautifulSoup(product_page.text, 'html.parser')
+    spec_rows = bs.find_all('tr',{'class':'specs__row'})
+    features = {}
+    for spec_row in spec_rows:
+        feature = spec_row.td.text
+        value = spec_row.p.text
+        features[feature] = value
+
+    print(product_page)
+    print(features)
+    time.sleep(1)
+
