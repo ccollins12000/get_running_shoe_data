99a3084b55854221c68ba4789aa88fa40ab3a9c0
Charles Collins <coll5631@stthomas.edu>
2020-09-01 15:42:45 -0500

    Combined function for additional shoe details
    Created a single function for reading a individual detail about a shoe from the shoe's page

--- get_shoe_data.py
+++ get_shoe_data.py
@@ -4,6 +4,29 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 
+detail_expressions = {
+    'Name': 'product--meta__title.*',
+    'Price': 'product--meta__price.*',
+    'Type':  'product--meta__sub-title.*'
+}
+
+def get_detail(shoe_page, expression):
+    """
+    Get a specific detail about the shoe
+
+    Args:
+        shoe_page (obj): The shoe page as parsed by get_shoe_page function
+        expression (str): Regular expression for value of HTML element class
+
+    Returns:
+        str: returns the detail about the shoe
+    """
+    try:
+        price = shoe_page.find(re.compile('.*'), {'class': re.compile(expression)}).text
+        return price
+    except AttributeError:
+        return None
+
 # Get all shoes
 brooks = requests.get('https://www.brooksrunning.com/en_us/shoes/?&sz=300')
 bs = BeautifulSoup(brooks.text, 'html.parser')
@@ -16,56 +39,10 @@ product_links = []
 for product in products:
     product_links.append(product.find('a')['href'])
 
+
 def get_shoe_page(shoe_url):
     shoe_page = requests.get(product_link)
     return BeautifulSoup(shoe_page.text, 'html.parser')
-
-
-def get_shoe_name(shoe_page):
-    """
-    Get the name of the shoe
-
-    Args:
-        shoe_page (obj): The shoe page as parsed by get_shoe_page function
-
-    Returns:
-        str: returns the name of the shoe
-    """
-    return shoe_page.find(re.compile('.*'), {'class': re.compile('product--meta__title.*')}).text
-
-
-def get_shoe_type(shoe_page):
-    """
-    Get the type of shoe
-
-    Args:
-        shoe_page (obj): The shoe page as parsed by get_shoe_page function
-
-    Returns:
-        str: returns the type of shoe
-    """
-    try:
-        return shoe_page.find(re.compile('.*'), {'class': re.compile('product--meta__sub-title.*')}).text
-    except AttributeError:
-        return None
-
-
-def get_shoe_price(shoe_page):
-    """
-    Get the price of the shoe
-
-    Args:
-        shoe_page (obj): The shoe page as parsed by get_shoe_page function
-
-    Returns:
-        str: returns the price of the shoe
-    """
-    try:
-        price = shoe_page.find(re.compile('.*'), {'class': re.compile('product--meta__price.*')}).text
-        return price
-    except AttributeError:
-        return None
-
 
 
 def get_shoe_specs(shoe_page):
@@ -97,21 +74,21 @@ def get_shoe_specs(shoe_page):
 # Retrieve details for each product
 all_shoes = list()
 loops = 0
-# loop_max = 9
+loop_max = 2
 for product_link in product_links:
     shoe_page = get_shoe_page(product_link)
     shoe_features = get_shoe_specs(shoe_page)
-    shoe_features['name'] = get_shoe_name(shoe_page)
-    shoe_features['price'] = get_shoe_price(shoe_page)
-    shoe_features['type'] = get_shoe_type(shoe_page)
-    shoe_details = pd.DataFrame(shoe_features, index=[shoe_features['name']])
+    for detail in detail_expressions:
+        shoe_features[detail] = get_detail(shoe_page, detail_expressions[detail])
+    shoe_details = pd.DataFrame(shoe_features, index=[shoe_features['Name']])
     all_shoes.append(shoe_details)
     time.sleep(1)
     loops += 1
-    # if loops > loop_max:
-    #    break
+    if loops > loop_max:
+        break
     print(str(loops), ' of', str(len(product_links)))
 
 
 all_shoes = pd.concat(all_shoes)
-all_shoes.to_csv('shoe_details.csv')
+print(all_shoes)
+# all_shoes.to_csv('shoe_details.csv')
